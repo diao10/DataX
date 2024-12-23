c0da446e6c902fcb8786b9f35afa08a93d2c8053
刁一宁 <12122319+boring_day@user.noreply.gitee.com>
2024-12-23 14:28:16 +0800

    update

--- datax-mysql/src/test/java/com/alibaba/datax/MysqlReaderMysqlWriterTest.java
+++ datax-mysql/src/test/java/com/alibaba/datax/MysqlReaderMysqlWriterTest.java
@@ -1,18 +1,18 @@
-package com.alibaba.datax;
-
-import com.alibaba.datax.util.PathUtil;
-import org.junit.Test;
-
-/**
- * {@code Author} FuYouJ
- * {@code Date} 2023/8/14 20:16
- */
-
-public class MysqlReaderMysqlWriterTest {
-    @Test
-    public void testMysqlReaderMysqlWriterTest() {
-        String path = "/demo.json";
-        String jobPath = PathUtil.getAbsolutePathFromClassPath(path);
-        ExampleContainer.start(jobPath);
-    }
-}
+//package com.alibaba.datax;
+//
+//import com.alibaba.datax.util.PathUtil;
+//import org.junit.Test;
+//
+///**
+// * {@code Author} FuYouJ
+// * {@code Date} 2023/8/14 20:16
+// */
+//
+//public class MysqlReaderMysqlWriterTest {
+//    @Test
+//    public void testMysqlReaderMysqlWriterTest() {
+//        String path = "/demo.json";
+//        String jobPath = PathUtil.getAbsolutePathFromClassPath(path);
+//        ExampleContainer.start(jobPath);
+//    }
+//}
